aaa24ec6ac249f9e6ab192cd164ac30da24ea800
galexander77 <87527303+gAlexander77@users.noreply.github.com>
2023-05-19 22:03:33 -0500

    Update

--- src/pages/Home/components/Projects.tsx
+++ src/pages/Home/components/Projects.tsx
@@ -1,14 +1,55 @@
 import React, { FC } from 'react';
+import projectsData from '../data/projects.json';
+import '../../../styles/Home/componets/Projects.css';
 
 const Projects: FC = () => {
+    
+    const projects = projectsData;
+    console.log(projects);
+
     return(
         <>
-            <h1>Projects</h1>
-            <div>
-                <div>Project 1</div>
-                <div>Project 2</div>
-            </div>
+            <h1 className="projects-title">Projects</h1>
+            <ListOfProjects projects={projects}/>
         </>
+    );
+}
+
+interface Project {
+    name: string;
+    techStack: string[];
+}
+
+interface ListofProjectsProps {
+    projects: Project[];
+}
+
+const ListOfProjects: FC<ListofProjectsProps> = ({projects}) => {
+    return(
+        <div className="list-of-projects-container">
+            {projects.map((project, index) =>(
+                <IndividualProject key={index} name={project.name} techStack={project.techStack}/>
+            ))}
+        </div>
+    );
+}
+
+interface IndividualProjectProps {
+    name: string;
+    techStack: string[];
+}
+
+const IndividualProject: FC<IndividualProjectProps> = ({name, techStack}) => {
+    return(
+        <div className="individual-project-container">
+            <h1>{name}</h1>
+            <div className="tech-stack-container">
+                {techStack.map((tech, index)=>(
+                    <p key={index}>{tech}</p>
+                ))}
+            </div>
+            <div className="individual-project-animated-background"/>
+        </div>
     );
 }
 
---
+++ src/pages/Project/Project.tsx
@@ -0,0 +1,27 @@
+import React, { FC } from 'react';
+import Navbar from '../Home/components/Navbar';
+
+const Project: FC = () => {
+    return(
+        <>
+            <RenderPage/>
+        </>
+    );
+}
+
+const RenderPage: FC = () => {
+    return(
+        <>
+        </>
+    );
+}
+
+// Types: Title, Text, Image, Gif, Video, Links
+const RenderType: FC = () => {
+    return(
+        <>
+        </>
+    );
+}
+
+export default Project;
