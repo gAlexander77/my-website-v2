3a042c2c497ad9138f37138096f33f1f95e14c36
galexander77 <87527303+gAlexander77@users.noreply.github.com>
2023-07-03 22:55:00 -0500

    Update

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home/Home';
+import Project from './pages/Project/Project';
 
 const App: React.FC = () => {
     return (
     <Router>
       <Routes>
         <Route path="/" element={<Home/>}/>
+        <Route path="/project" element={<Project/>}/>
       </Routes>
     </Router>
     );
